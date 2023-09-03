c3d6c70714ea12d592f18ae092a6632332df90f6
Hugo Persson <hugo.e.persson@gmail.com>
2023-09-03 22:46:28 +0200

    Started work, found way to get image just need to succeed with sending it

--- main.ts
+++ main.ts
@@ -1,57 +1,81 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+} from "obsidian";
+
+// @ts-ignore "electron" is installed.
+import { clipboard } from "electron";
 
 // Remember to rename these classes and interfaces!
 
 interface MyPluginSettings {
-	mySetting: string;
+	ocrBaseUrl: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+	ocrBaseUrl: "",
+};
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
+	async generateLatexFromClipboardImage() {
+		//let buffer = clipboard.readImage();
+		//let buffer = clipboard.readBuffer();
+		//console.log(buffer);
+		console.log(clipboard.availableFormats());
+		console.log(clipboard.readImage());
+		if (clipboard.availableFormats()[0] == "image/png") {
+			let buffer = clipboard.readBuffer("image/png");
+			let formData = new FormData();
+			formData.append("file", buffer);
+			const url = this.settings.ocrBaseUrl + "/buffer/";
+			let response = await fetch(url, {
+				method: "POST",
+				body: formData,
+			});
+			let latex = await response.text();
+			console.log(latex);
+			console.log(buffer);
+		}
+		return "";
+	}
+
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
 		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
+			id: "generate-latex-from-last-image",
+			name: "Generate latex from last image to clipboard",
 			callback: () => {
-				new SampleModal(this.app).open();
-			}
+				let latex = this.generateLatexFromClipboardImage();
+				alert(latex);
+			},
 		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: "sample-editor-command",
+			name: "Sample editor command",
+			editorCallback: async (editor: Editor, view: MarkdownView) => {
+				console.log(await editor.getSelection());
+				editor.replaceSelection("Sample Editor Command");
+			},
 		});
 		// This adds a complex command that can check whether the current state of the app allows execution of the command
 		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "open-sample-modal-complex",
+			name: "Open sample modal (complex)",
 			checkCallback: (checking: boolean) => {
 				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+				const markdownView =
+					this.app.workspace.getActiveViewOfType(MarkdownView);
 				if (markdownView) {
 					// If checking is true, we're simply "checking" if the command can be run.
 					// If checking is false, then we want to actually perform the operation.
@@ -62,7 +86,7 @@ export default class MyPlugin extends Plugin {
 					// This command will only show up in Command Palette when the check function returns true
 					return true;
 				}
-			}
+			},
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -70,20 +94,24 @@ export default class MyPlugin extends Plugin {
 
 		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
 		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
+			console.log("click", evt);
 		});
 
 		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.registerInterval(
+			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000)
+		);
 	}
 
-	onunload() {
-
-	}
+	onunload() {}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
@@ -97,12 +125,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.setText("Woah!");
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,19 +144,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("OCR BASE_URL")
+			.setDesc("The base url for your OCR server")
+			.addText((text) =>
+				text
+					.setPlaceholder("Enter your BASE_URL")
+					.setValue(this.plugin.settings.ocrBaseUrl)
+					.onChange(async (value) => {
+						this.plugin.settings.ocrBaseUrl = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
