b8e591b069e618a57476b926f5736fbd687fbbfa
Hugo Persson <hugo.e.persson@gmail.com>
2024-03-10 13:33:00 +0100

    Updated version and added do release script

---
+++ scripts/do-release.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+last_tag=$(git describe --tags --abbrev=0)
+echo "Last tag: $last_tag"
+# Prompt the user for a version number
+read -p "Enter the version number: " version_number
+
+# Tag the last git commit with the provided version number
+git tag -a "$version_number" -m "$version_number"
+
+# Push the tagged commit to the remote repository
+git push origin "$version_number"
+
+echo "Tagged and pushed commit with version $version_number successfully."
